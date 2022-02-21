441f2b4f8d659a511c6f72bfc69956091e078bfc
eagr <eagr@tutanota.com>
2022-02-22 00:24:07 +0800

    OpenSea factory interfaces

---
+++ interface/OpenSeaFactory1155.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface OpenSeaFactory1155 {
+    function name() external view returns (string memory);
+
+    function symbol() external view returns (string memory);
+
+    function numOptions() external view returns (uint256);
+
+    function uri(uint256 optionId) external view returns (string memory);
+
+    function factorySchemaName() external view returns (string memory);
+
+    function supportsFactoryInterface() external view returns (bool);
+
+    function canMint(uint256 optionId, uint256 amount) external view returns (bool);
+
+    function mint(uint256 optionId, address to, uint256 amount, bytes calldata data) external;
+
+    function isApprovedForAll(address owner, address operator) external view returns (bool);
+
+    function safeTransferFrom(address from, address to, uint256 optionId, uint256 amount, bytes calldata data) external;
+
+    function balanceOf(address account, uint256 optionId) external view returns (uint256);
+}
---
+++ interface/OpenSeaFactory721.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface OpenSeaFactory721 {
+    function name() external view returns (string memory);
+
+    function symbol() external view returns (string memory);
+
+    function numOptions() external view returns (uint256);
+
+    function tokenURI(uint256 optionId) external view returns (string memory);
+
+    function supportsFactoryInterface() external view returns (bool);
+
+    function canMint(uint256 optionId) external view returns (bool);
+
+    function mint(uint256 optionId, address to) external;
+}
