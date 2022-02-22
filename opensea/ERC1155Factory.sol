694a524d2d36b2e2709421804e6a7e48bc22f4f1
eagr <eagr@tutanota.com>
2022-02-22 22:10:42 +0800

    remove standardized interfaces

--- opensea/ERC1155Factory.sol
+++ opensea/ERC1155Factory.sol
@@ -2,25 +2,13 @@
 pragma solidity ^0.8.0;
 
 interface OpenSeaERC1155Factory {
-    function name() external view returns (string memory);
-
-    function symbol() external view returns (string memory);
-
-    function numOptions() external view returns (uint256);
-
-    function uri(uint256 optionId) external view returns (string memory);
+    function supportsFactoryInterface() external view returns (bool);
 
     function factorySchemaName() external view returns (string memory);
 
-    function supportsFactoryInterface() external view returns (bool);
+    function numOptions() external view returns (uint256);
 
     function canMint(uint256 optionId, uint256 amount) external view returns (bool);
 
     function mint(uint256 optionId, address to, uint256 amount, bytes calldata data) external;
-
-    function isApprovedForAll(address owner, address operator) external view returns (bool);
-
-    function safeTransferFrom(address from, address to, uint256 optionId, uint256 amount, bytes calldata data) external;
-
-    function balanceOf(address account, uint256 optionId) external view returns (uint256);
 }
--- opensea/ERC721Factory.sol
+++ opensea/ERC721Factory.sol
@@ -2,15 +2,9 @@
 pragma solidity ^0.8.0;
 
 interface OpenSeaERC721Factory {
-    function name() external view returns (string memory);
-
-    function symbol() external view returns (string memory);
+    function supportsFactoryInterface() external view returns (bool);
 
     function numOptions() external view returns (uint256);
-
-    function tokenURI(uint256 optionId) external view returns (string memory);
-
-    function supportsFactoryInterface() external view returns (bool);
 
     function canMint(uint256 optionId) external view returns (bool);
 
