faafb599810c629bdf87c3d277b08390c919fe91
eagr <eagr@tutanota.com>
2022-02-22 11:48:48 +0800

    rename

--- interface/opensea/FactoryERC1155.sol
+++ interface/opensea/FactoryERC1155.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-interface OpenSeaFactory1155 {
+interface OpenSeaFactoryERC1155 {
     function name() external view returns (string memory);
 
     function symbol() external view returns (string memory);
--- interface/opensea/FactoryERC721.sol
+++ interface/opensea/FactoryERC721.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-interface OpenSeaFactory721 {
+interface OpenSeaFactoryERC721 {
     function name() external view returns (string memory);
 
     function symbol() external view returns (string memory);
